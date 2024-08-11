2766a671961cfac33e675cb1b9cad343fb83a0fa
CatBoxy <catboxy@gmail.com>
2024-08-11 21:50:46 +0800

    #1 模組初建 - 建立 vue + bootstrap5 + vite 模組 & modal

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# template-vue-vite-bootstrap
+# vue + vite + bootstrap 模板 by CatBoxy
+- 已寫好用 JS 控 bootstrap modal 開關的方法。
+- 因 sass 1.77.8 有 bug，降板至 1.77.6
+
+## modules - dependencies
+- @popperjs/core: "^2.11.8",
+- bootstrap: "^5.3.3",
+- pinia: "^2.1.7",
+- vue: "^3.4.29",
+- vue-router: "^4.3.3"
+
+## modules - devDependencies
+- @rushstack/eslint-patch: "^1.8.0",
+- @vitejs/plugin-vue: "^5.0.5",
+- @vue/eslint-config-prettier: "^9.0.0",
+- eslint: "^8.57.0",
+- eslint-plugin-vue: "^9.23.0",
+- prettier: "^3.2.5",
+- sass: "^1.77.6",
+- vite: "^5.3.1"
---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+// 法 1 - 只引入需要的功能
+// import { Modal } from 'bootstrap';
+// const myModal = ref(null);
+
+// onMounted(() => {
+//   const test = new Modal(myModal.value);
+//   test.show();
+// })
+
+// 法 2 - 引入全部的 bootstrap
+// import * as bootstrap from 'bootstrap';
+// const myModal = ref(null);
+
+// onMounted(() => {
+//   const test = new bootstrap.Modal(myModal.value, {});
+//   test.show();
+// });
+
+// 法 3 - 跟法 1 有點像，只是引入檔案的路徑更明確
+// import * as bootstrap from 'bootstrap'; // 依需求決定是否引入全部
+import Modal from 'bootstrap/js/dist/modal';
+const myModal = ref(null);
+
+onMounted(() => {
+  const test = new Modal(myModal.value);
+  test.show();
+});
+</script>
+
+<template>
+  <main>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+      ref="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
